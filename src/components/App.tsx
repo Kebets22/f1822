430abdc75b0813035dad152eb8aa8194c84e5a5e
Kebets22 <kebets22@icloud.com>
2022-11-15 18:10:59 +0300

    create search of username

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,10 @@
 import './App.css';
-<<<<<<< HEAD
-import Calculator from './Calculator/Calculator';
-
-
-function App() {
-  return(
-    <Calculator/>
-  )   
-};
-=======
 import UserCards from './Users/UserCards';
 
 function App() {
-  return (
-    <div className=''>
+  return(
     <UserCards/>
-    </div>
-  );
+  )
 }
->>>>>>> 1882de80f007f4180710599835aa6093486a4d7d
 
 export default App;
--- src/components/Users/UserCards.tsx
+++ src/components/Users/UserCards.tsx
@@ -3,11 +3,30 @@ import {USERS} from './users';
 
 const UserCards = () => {
     const [users, setUsers] = useState(USERS);
+    const [search, setSearch] = useState('');
     const deleteUser = (id: number) => {
         setUsers(users.filter(user => user.id !== id));
     }
+    const searchedUsers = () =>{
+        if(search){
+            return users.filter(user => user.username.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+        }
+        return users;
+    };
+    console.log(searchedUsers())
     return(
         <div className="row row-cols-1 row-cols-md-3 g-4 mt-5">
+            <h1 className='text-center w-100'>User cards</h1>
+            <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Search</span>
+            <input type="text" 
+            className="form-control" 
+            placeholder="Input Username" 
+            aria-label="Username" 
+            aria-describedby="basic-addon1"
+            onChange={(event) =>  setSearch(event.target.value)}
+            />
+            </div>
             {users.map(user => <div className="col" key={user.id}>
             <div className="card h-100">
                 <div className="card-body">
